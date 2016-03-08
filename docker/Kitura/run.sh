ea707baba0a12fb1a45123984dec69dc9fc023b8
Krzysztof Siejkowski <krzysztof@siejkowski.net>
2016-03-08 18:17:18 +0100

    updated dockerfile, now it enables vncing to clion inside

--- docker/Kitura/run.sh
+++ docker/Kitura/run.sh
@@ -3,4 +3,4 @@
 docker stop kitura-sample
 docker rm -f kitura-sample
 
-docker run -it --privileged=true -v "$1":/project --name kitura-sample swift-for-backend:kitura /bin/bash
+docker run -it -p 5900:5900 --privileged=true -v "$1":/project --name kitura-sample swift-for-backend:kitura 

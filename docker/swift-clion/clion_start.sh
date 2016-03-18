8fac0ab74dd6c2c995e7bc366e0c0279b246583c
Krzysztof Siejkowski <krzysztof@siejkowski.net>
2016-03-18 22:21:38 +0100

    keymapping and some other goodies

--- docker/swift-clion/clion_start.sh
+++ docker/swift-clion/clion_start.sh
@@ -3,9 +3,11 @@
 mkdir -p /root/.CLion2016.1/config/plugins
 mkdir -p /root/.CLion2016.1/config/options
 cp /clion-swift-145.256.37.jar /root/.CLion2016.1/config/plugins/
-echo '<application>\n  <component name="EditorColorsManagerImpl">\n    <global_color_scheme name="Darcula" />\n  </component>\n</application>' >> /root/.CLion2016.1/config/options/colors.scheme.xml
-echo '<application>\n  <component name="EditorSettings">\n    <option name="ARE_LINE_NUMBERS_SHOWN" value="true" />\n  </component>\n</application>' >> /root/.CLion2016.1/config/options/editor.xml
-echo '<application>\n  <component name="LafManager">\n    <laf class-name="com.intellij.ide.ui.laf.darcula.DarculaLaf" />\n  </component>\n</application>' >> /root/.CLion2016.1/config/options/laf.xml
+cp -r /IdeaVim /root/.CLion2016.1/config/plugins/
+echo '<application><component name="EditorColorsManagerImpl"><global_color_scheme name="Darcula" /></component></application>' > /root/.CLion2016.1/config/options/colors.scheme.xml
+echo '<application><component name="EditorSettings"><option name="ARE_LINE_NUMBERS_SHOWN" value="true" /></component></application>' > /root/.CLion2016.1/config/options/editor.xml
+echo '<application><component name="LafManager"><laf class-name="com.intellij.ide.ui.laf.darcula.DarculaLaf" /></component></application>' > /root/.CLion2016.1/config/options/laf.xml
+echo '<application><component name="KeymapManager"><active_keymap name="Mac OS X 10.5+" /></component></application>' > /root/.CLion2016.1/config/options/keymap.xml
 cd clion-145.256.37/bin 
 ./clion.sh
 

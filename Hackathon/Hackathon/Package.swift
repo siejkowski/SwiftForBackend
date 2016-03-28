9976d0de23c1a4469171fe01904ce8b036f9f26d
Krzysztof Siejkowski <krzysztof@siejkowski.net>
2016-03-28 11:07:47 +0200

    hackathon project with spm

--- Hackathon/Hackathon/Package.swift
+++ Hackathon/Hackathon/Package.swift
@@ -1,9 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "hello",
+    name: "Hackathon",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/Zewo.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 3),
     ]
 )
